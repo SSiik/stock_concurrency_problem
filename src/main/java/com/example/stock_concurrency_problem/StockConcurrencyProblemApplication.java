9a6c191e0affe5f21b1b36f5d7ac61b6b8678cb3
SSiik <sik_5571@naver.com>
2022-08-17 20:51:22 +0900

    first setting commit

---
+++ src/main/java/com/example/stock_concurrency_problem/StockConcurrencyProblemApplication.java
@@ -0,0 +1,13 @@
+package com.example.stock_concurrency_problem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockConcurrencyProblemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockConcurrencyProblemApplication.class, args);
+	}
+
+}
