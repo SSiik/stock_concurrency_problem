3c91c9fe504fad2017321be88922daf5012af35c
SSiik <sik_5571@naver.com>
2022-08-18 16:39:06 +0900

    Solve with optimistic Lock

--- src/main/java/com/example/stock_concurrency_problem/repository/stockRepository.java
+++ src/main/java/com/example/stock_concurrency_problem/repository/stockRepository.java
@@ -2,6 +2,14 @@ package com.example.stock_concurrency_problem.repository;
 
 import com.example.stock_concurrency_problem.domain.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.persistence.LockModeType;
 
 public interface stockRepository extends JpaRepository<Stock,Long> {
+
+    @Lock(value = LockModeType.OPTIMISTIC)
+    @Query("select s from Stock s where s.id = :id")
+    Stock findByIdWithOptimisticLock(Long id); 
 }
